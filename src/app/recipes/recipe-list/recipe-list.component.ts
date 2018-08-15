5caa9cfb8f151c7e5cd909941c69109e5c97ccbf
Obe <obe@inergent.com>
2018-08-14 19:54:40 -0500

    Update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is my test recipe", "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg"),
-    new Recipe("A Test Recipe", "This is my test recipe", "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg")
+    new Recipe("Another Test Recipe", "This is my other test recipe", "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
