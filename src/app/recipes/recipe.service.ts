fb0d8125411b47eaaa208c8f816728875fc6cb01
Obe <obe@inergent.com>
2018-08-17 16:19:16 -0500

    Adding Routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is my test recipe", "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg"),
-        new Recipe("Another Test Recipe", "This is my other test recipe", "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg")
+        new Recipe(
+            "New Orleans Gumbo",
+            "Best gumbo you have ever tasted!",
+            "https://pinchofyum.com/wp-content/uploads/Gumbo-1-2-600x900.jpg",
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fires', 20)
+            ]),
+        new Recipe(
+            "Burger Time",
+            "A burger like no other!",
+            "https://www.gnom-gnom.com/wp-content/uploads/2018/08/Gluten-Free-Paleo-Keto-Burger-Buns-by-Gnom-Gnom-2.jpg",
+            [
+               new Ingredient('Buns', 2),
+               new Ingredient('Meat', 1) 
+            ])
       ];
 
+    constructor(private slService: ShoppingListService) {}
+    
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
